0ae8013c356ed0c457499d1dea0c39341208e0e7
majdarkilo <majd.arkilo@gmail.com>
2018-11-16 21:37:36 +0000

    updated blink file with for loops

--- blink5.sh
+++ blink5.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 for i in 1 2 3 4 5; do
-	gpio -g write 17 1
-	gpio -g write 18 1
-	gpio -g write 27 1
-	gpio -g write 22 1
+	for j in 17 18 27 22; do
+		gpio -g write $j 1
+	done
 	sleep 1 
-	gpio -g write 17 0
-	gpio -g write 18 0
-	gpio -g write 27 0
-	gpio -g write 22 0
+	for p in 22 27 18 17; do 
+		gpio -g write $p 0
+	done
 	sleep 1 
 done 
